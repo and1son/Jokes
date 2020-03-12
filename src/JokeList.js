727f2b36ece7af0bb45af126166c18bf00401ebd
Andi Bašić <andi.basic10@gmail.com>
2020-03-12 10:54:22 +0100

    Loading Spinner

--- src/JokeList.js
+++ src/JokeList.js
@@ -14,7 +14,8 @@ class JokeList extends Component{
         super(props)
     
         this.state = {
-            jokes : JSON.parse(window.localStorage.getItem("jokes") || "[]")         
+            jokes : JSON.parse(window.localStorage.getItem("jokes") || "[]")  ,
+            loading : false,       
         }
         this.handleClick = this.handleClick.bind(this)
     }
@@ -32,6 +33,7 @@ class JokeList extends Component{
             jokes.push({id: uuid(), text: res.data.joke, votes: 0})
         }
         this.setState( oldstate => ({
+            loading : false,
             jokes : [...oldstate.jokes, ...jokes]
         }),
         () => window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes)) )
@@ -48,11 +50,20 @@ class JokeList extends Component{
     }
 
     handleClick(){
-        this.getJokes()
+        //callback this.getJokes
+        this.setState({loading: true}, this.getJokes)
     }
 
 
     render(){
+        if (this.state.loading){
+            return(
+                <div className="JokeList-spinner">
+                    <i className="far fa-8x fa-laugh fa-spin" />
+                    <h1 className="JokeList-title">Loading...</h1>
+                </div>
+            )
+        }
         return(
             <div className="JokeList">
                 <div className="JokeList-sidebar">
